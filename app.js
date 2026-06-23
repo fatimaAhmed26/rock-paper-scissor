123af884dfb00da13da31f405ab7bde686d36a80
fatimaAhmed26 <fatima.bh2248@gmail.com>
2026-06-23 10:39:04 +0300

    adding the reset button

--- app.js
+++ app.js
@@ -5,14 +5,37 @@ let userChoice =''
 
 /*------------------------ Cached Element References ------------------------*/
 const choices =document.querySelector('#choices')
-
+const rock = document.querySelector('#Rock')
+const paper = document.querySelector('#Paper')
+const Scissor = document.querySelector('#Scissor')
+const reset =document.querySelector('#reset')
 
 /*----------------------------- Event Listeners -----------------------------*/
 choices.addEventListener('click', function(Event){
     userChoice = event.target.id
+
+    //hide the choices are not user choice 
+    if(userChoice === 'Rock'){
+        rock.classList.add('hidden')
+        console.log('user chose rock')
+    }if(userChoice === 'Paper'){
+         paper.classList.add('hidden')
+        console.log('user chose paper ')
+    }
+    if(userChoice === 'Scissor'){
+        Scissor.classList.add('hidden')
+        console.log('user chose Scissor ')
+    }
     console.log('user choice: ' + userChoice)
     console.log(event.target.id)
-    
+})
+
+//reset button 
+reset.addEventListener('click', function(){
+    paper.classList.remove('hidden')
+    rock.classList.remove('hidden')
+    Scissor.classList.remove('hidden')
+
 })
 
 /*-------------------------------- Functions --------------------------------*/
